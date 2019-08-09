0d195b9fbc451aefc855de667ccb2b5594f9575a
Marshall <limitshel@live.com>
2019-08-09 17:21:50 +0800

    Initial

--- pisces-engine/src/main/java/com/stormragetech/pisces/flink/engine/app/Launch.java
+++ pisces-engine/src/main/java/com/stormragetech/pisces/flink/engine/app/Launch.java
@@ -1,42 +1,115 @@
 package com.stormragetech.pisces.flink.engine.app;
 
-import org.apache.flink.api.common.functions.JoinFunction;
-import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.state.BroadcastState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.HeapBroadcastState;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.BroadcastStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.util.Iterator;
+import java.util.Map;
 
 public class Launch {
 
     public static void main(String[] args) {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        DataStreamSource<String> source1 = env.readTextFile("/Users/marshall/Desktop/a.txt");
-        DataStreamSource<String> source2 = env.readTextFile("/Users/marshall/Desktop/b.txt");
-
+        env.setParallelism(4);
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        DataStream<String> stream1 = source1
-                .map((MapFunction<String, String>) s -> null == s || s.trim().length() <= 0 ? "null" : s.trim())
-                .assignTimestampsAndWatermarks(Utils.assignerWithPeriodicWatermarks());
-        stream1.print("stream [1] >>>");
+        final MapStateDescriptor<String, String> CONFIG_DESCRIPTOR = new MapStateDescriptor<>(
+                "label",
+                BasicTypeInfo.STRING_TYPE_INFO,
+                BasicTypeInfo.STRING_TYPE_INFO
+        );
 
-        DataStream<String> stream2 = source1
-                .map((MapFunction<String, String>) s -> null == s || s.trim().length() <= 0 ? "null" : s.trim())
-                .forward()
-                .assignTimestampsAndWatermarks(Utils.assignerWithPeriodicWatermarks());
-        stream2.print("stream [2] >>>");
+//        DataStreamSource<String> source1 = env.readTextFile("/Users/marshall/Desktop/a.txt");
+        BroadcastStream<String> broadcastStream = env
+                .readTextFile("/Users/marshall/Desktop/a.txt")
+                .setParallelism(1)
+                .broadcast(CONFIG_DESCRIPTOR);
 
-        stream1.join(stream2)
-                .where(String::trim).equalTo(String::trim)
-                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
-                .trigger(CountTrigger.of(1))
-                .apply((JoinFunction<String, String, String>) (s, s2) -> s + " || " + s2).print("result>>>");
+//        try {
+//            Thread.sleep(1000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        DataStreamSource<String> source2 = env.readTextFile("/Users/marshall/Desktop/b.txt");
+//        source2.print("source2 >>> ");
+
+        DataStream<String> result = source2.connect(broadcastStream)
+                .process(new BroadcastProcessFunction<String, String, String>() {
+
+                    private String label = null;
+
+//                    @Override
+//                    public void open(Configuration parameters) throws Exception {
+//                        super.open(parameters);
+//                        label = "java";
+//                        System.out.println("初始化模拟连接数据库读取拦截关键字：java");
+//                    }
+
+                    @Override
+                    public void processElement(String s, ReadOnlyContext readOnlyContext, Collector<String> collector) throws Exception {
+
+//                        System.out.println("process : " + s + " == " + label);
+
+//                        if (null != s && null != label && s.startsWith(label)) {
+//                            collector.collect(s + " || " + label);
+//                        }
+
+//                        System.out.println("no broadcast 1 >>> [ " + s + " ]");
+                        HeapBroadcastState<String, String> config = (HeapBroadcastState<String, String>) readOnlyContext.getBroadcastState(CONFIG_DESCRIPTOR);
+//                        System.out.println(config);
+                        Iterator<Map.Entry<String, String>> iterator = config.iterator();
+                        while (iterator.hasNext()) {
+                            String result = "null";
+                            Map.Entry<String, String> entry = iterator.next();
+//                            System.out.println(s + " ^^ " + entry.getValue() + " ^^ " + s.startsWith(entry.getValue()));
+                            if (null != entry.getValue() && s.startsWith(entry.getValue())) {
+                                result = s + " || " + entry.getValue();
+                            }
+                            if (!result.equals("null")) {
+                                collector.collect(result);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void processBroadcastElement(String s, Context context, Collector<String> collector) throws Exception {
+                        BroadcastState<String, String> state = context.getBroadcastState(CONFIG_DESCRIPTOR);
+                        if (null == state.get(s)) {
+                            state.put(s, s);
+                        }
+//                        System.out.println(state);
+                    }
+                });
+
+
+//        DataStream<String> stream1 = source1
+//                .map((MapFunction<String, String>) s -> null == s || s.trim().length() <= 0 ? "null" : s.trim())
+//                .assignTimestampsAndWatermarks(Utils.assignerWithPeriodicWatermarks());
+//        stream1.print("stream [1] >>>");
+//
+//        DataStream<String> stream2 = source1
+//                .map((MapFunction<String, String>) s -> null == s || s.trim().length() <= 0 ? "null" : s.trim())
+//                .forward()
+//                .assignTimestampsAndWatermarks(Utils.assignerWithPeriodicWatermarks());
+//        stream2.print("stream [2] >>>");
+//
+//        stream1.join(stream2)
+//                .where(String::trim).equalTo(String::trim)
+//                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+//                .trigger(CountTrigger.of(1))
+//                .apply((JoinFunction<String, String, String>) (s, s2) -> s + " || " + s2).print("result >>>");
 
 
         //        DataStream<String> stream2 = source2.map((MapFunction<String, String>) s -> null == s ? "null" : s.trim());
@@ -72,7 +145,7 @@ public class Launch {
 //                    }
 //                });
 
-//        result.print("result >>>");
+        result.print("result >>>");
 
         try {
             env.execute();
